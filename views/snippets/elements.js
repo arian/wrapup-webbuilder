a8c8b1262440964fd195ee0193c0256b99bb35cf
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-11-07 04:29:32 +0100

    Add semicolons to the snippets, the editor likes them.

--- views/snippets/elements.js
+++ views/snippets/elements.js
@@ -2,15 +2,15 @@
 // export elements
 var elements = global.elements = exports.elements = require('elements');
 
-// require('elements/lib/attributes')
-// require('elements/lib/events')
-// require('elements/lib/insertion')
+// require('elements/lib/attributes');
+// require('elements/lib/events');
+// require('elements/lib/insertion');
 
-// elements.domready = require('elements/lib/domready')
+// elements.domready = require('elements/lib/domready');
 
 // these modules also depend on slick
 
-// require('elements/lib/traversal')
-// require('elements/lib/delegation')
+// require('elements/lib/traversal');
+// require('elements/lib/delegation');
 
-// elements.zen = require('elements/lib/zen')
+// elements.zen = require('elements/lib/zen');
--- views/snippets/prime.js
+++ views/snippets/prime.js
@@ -2,16 +2,16 @@
 // export prime
 var prime = global.prime = exports.prime = require('prime');
 
-// prime.hash    = require('prime/collection/hash')
-// prime.list    = require('prime/collection/list')
-// prime.map     = require('prime/collection/map')
+// prime.hash    = require('prime/collection/hash');
+// prime.list    = require('prime/collection/list');
+// prime.map     = require('prime/collection/map');
 
-// prime.array   = require('prime/es5/array')
-// prime.fn      = require('prime/es5/function')
-// prime.regexp  = require('prime/es5/regexp')
-// prime.string  = require('prime/types/string')
-// prime.number  = require('prime/types/number')
+// prime.array   = require('prime/es5/array');
+// prime.fn      = require('prime/es5/function');
+// prime.regexp  = require('prime/es5/regexp');
+// prime.string  = require('prime/types/string');
+// prime.number  = require('prime/types/number');
 
-// prime._       = require('prime/util/ghost')
-// prime.emitter = require('prime/util/emitter')
-// prime.type    = require('prime/util/type')
+// prime._       = require('prime/util/ghost');
+// prime.emitter = require('prime/util/emitter');
+// prime.type    = require('prime/util/type');
