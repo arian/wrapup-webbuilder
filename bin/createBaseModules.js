5839a4bb8690bb99ffacc6a3f7b083fa810e731d
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-06-22 14:08:48 +0200

    Improve bin/createBaseModules, only npm install when necessary.
    Also refactor the code, more functional and separated.

--- bin/createBaseModules.js
+++ bin/createBaseModules.js
@@ -5,7 +5,40 @@ var fs    = require('fs-extra');
 var path  = require('path');
 var async = require('async');
 var prime = require('prime');
-var exec  = require('child_process').exec;
+var spawn = require('child_process').spawn;
+
+function pkgNotExists(pkg, callback){
+	fs.exists(pkg.dir, function(exists){
+		callback(!exists);
+	});
+}
+
+function npmInstall(dir, callback){
+	spawn('npm', ['install'], {
+		cwd: dir,
+		stdio: 'inherit'
+	}).on('close', callback);
+}
+
+function install(pkg, callback){
+	async.series([
+		async.apply(fs.mkdirs, pkg.dir),
+		async.apply(fs.writeFile, pkg.dir + '/README.md', '# ' + pkg.name + '@' + pkg.version),
+		async.apply(fs.writeFile, pkg.dir + '/package.json', pkg.json),
+		async.apply(npmInstall, pkg.dir)
+	], callback);
+}
+
+function installAll(pkgs, callback){
+	async.map(pkgs, install, callback);
+}
+
+function pkgsToInstall(pkgs, callback){
+	async.filter(pkgs, pkgNotExists, callback.bind(null, null));
+}
+
+var installAllNonExisting = async.compose(installAll, pkgsToInstall);
+
 
 var modules, config, dir;
 try {
@@ -18,11 +51,12 @@ try {
 	process.exit(1);
 }
 
+var pkgs = [];
+
 prime.each(modules, function(versions, name){
 
-	async.forEach(versions, function(version, callback){
+	versions.forEach(function(version, callback){
 		var pkg = name + '@' + version;
-		var pkgDir = dir + '/' + pkg;
 		var json = {
 			name: name,
 			version: version,
@@ -31,25 +65,26 @@ prime.each(modules, function(versions, name){
 			repository: {}
 		};
 		json.dependencies[name] = version;
-		json = JSON.stringify(json, null, 2);
 
-		async.series([
-			async.apply(fs.mkdirs, pkgDir),
-			async.apply(fs.writeFile, pkgDir + '/README.md', '# ' + name + '@' + version),
-			async.apply(fs.writeFile, pkgDir + '/package.json', json),
-			async.apply(exec, 'npm install', {
-				cwd: pkgDir
-			})
-		], function(err, results){
-			if (err) throw err;
-			else {
-				console.log("All modules were created with the correct versions");
-				var stdout = results[3][0];
-				var stderr = results[3][1];
-				if (stdout) console.log(stdout);
-				if (stderr) console.error(stderr);
-			}
+		pkgs.push({
+			name: name,
+			version: version,
+			pkg: pkg,
+			dir: dir + '/' + pkg,
+			json: JSON.stringify(json, null, 2)
 		});
+
 	});
 
+});
+
+installAllNonExisting(pkgs, function(err, installed){
+	if (err) throw err;
+	if (installed.length){
+		installed.forEach(function(res){
+			console.log('installed "' + res[0] + '"');
+		});
+	} else {
+		console.log("didn't need to install any modules");
+	}
 });
